36598b9cd036f2f5f9f3c712b49594e8c61c352b
rjsanks <ralphsanks@debbies-imac.hsd1.co.comcast.net>
2014-08-01 17:32:29 -0600

    added a test class

---
+++ Git Test/RJSTest.h
@@ -0,0 +1,13 @@
+//
+//  RJSTest.h
+//  Git Test
+//
+//  Created by Ralph Sanks on 8/1/14.
+//  Copyright (c) 2014 InnovaCo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RJSTest : NSObject
+
+@end
---
+++ Git Test/RJSTest.m
@@ -0,0 +1,13 @@
+//
+//  RJSTest.m
+//  Git Test
+//
+//  Created by Ralph Sanks on 8/1/14.
+//  Copyright (c) 2014 InnovaCo. All rights reserved.
+//
+
+#import "RJSTest.h"
+
+@implementation RJSTest
+
+@end
